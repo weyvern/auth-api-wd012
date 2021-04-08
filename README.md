f266bc4d76b0d2412a0f0bd3efab28a0faedc176
Jorge Paul <jserranopaul@gmail.com>
2021-04-08 11:12:57 +0200

    Hash password and return JWT

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [x] Set up MongoDB
-[] Create my User model with Mongoose
-[] Create a new user -> /signup (returns token)
+[x] Create my User model with Mongoose
+[x] Create a new user -> POST /signup (returns token) + hashed password
 [] Login -> /signin (returns token)
 [] Get user info -> /me (returns user info if authenticated)
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,16 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import User from '../models/User.js';
 
 export const signUp = async (req, res) => {
   try {
     const { name, email, password, address } = req.body;
-    const newUser = await User.create({ name, email, password, address });
-    res.json(newUser);
+    const foundUser = await User.find({ email });
+    if (foundUser.length > 0) throw new Error('Email already taken');
+    const hashPassword = await bcrypt.hash(password, 5);
+    const { _id, name: userName } = await User.create({ name, email, password: hashPassword, address });
+    const token = jwt.sign({ _id, userName }, process.env.JWT_SECRET);
+    res.json({ token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
